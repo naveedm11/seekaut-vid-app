3d0b49f5c442e457d61a93244c83fed19af25dcf
shahbaz <shahbaz@devbeans.io>
2022-02-21 00:50:56 +0500

    liked by

--- app/controllers/videoCount.controller.js
+++ app/controllers/videoCount.controller.js
@@ -6,21 +6,21 @@ const ObjectId = require('mongodb').ObjectId;
 exports.like = async(req, res) => {
     video_id = req.params.vid_id
     if (!video_id) {
-        res.status(400).send({ status: "failed", message: "please provide the video id" })
+        res.status(400).send({ success: false, message: "please provide the video id" })
     }
     const videoCount = await VideoCount.findOne({ video: video_id })
     if (!videoCount) {
-        res.status(404).send({ status: "Failed", message: 'Video Not Found' })
+        res.status(404).send({ success: false, message: 'Video Not Found' })
     }
     
     let user_id = req.params.user_id;
     if (!user_id) {
-        res.status(400).send({ status: "failed", message: "please provide the user id" })
+        res.status(400).send({ success: false , message: "please provide the user id" })
     }
 
     const user = await User.findById(user_id)
     if (!user) {
-        res.status(404).send({ status: "Failed", message: 'User with the given id  Not Found' })
+        res.status(404).send({ success: false , message: 'User with the given id  Not Found' })
     }
 
     const index_of_like = videoCount.likedBy.indexOf(String(user_id));
@@ -37,33 +37,30 @@ exports.like = async(req, res) => {
         videoCount.likedBy.push(user_id )
       res.status(400).send({ success: true , message: "liked successfully"});
   }
-
-    await videoCount.save()
-    res.status(201).send({ status: "success" })
 }
 
 exports.comment = async(req, res) => {
     try {
         video_id = req.body.video_id
         if (!video_id) {
-            res.status(400).send({ status: "failed", message: "please provide the video id" })
+            res.status(400).send({ success: false , message: "please provide the video id" })
         }
 
         let comment = req.body.comment
         if (!comment) {
-            res.status(400).send({ status: "failed", message: "please provide the Comment" })
+            res.status(400).send({ success: false , message: "please provide the Comment" })
         }
         let user_id = req.body.user_id;
         if (!user_id) {
-            res.status(400).send({ status: "failed", message: "please provide the user id" })
+            res.status(400).send({ success: false , message: "please provide the user id" })
         }
         const videoCount = await VideoCount.findOne({ video: video_id })
         if (!videoCount) {
-            res.status(404).send({ status: "Failed", message: 'Video with the given id Not Found' })
+            res.status(404).send({ success: false , message: 'Video with the given id Not Found' })
         }
         const user = await User.findById(user_id)
         if (!user) {
-            res.status(404).send({ status: "Failed", message: 'User with the given id  Not Found' })
+            res.status(404).send({ success: false , message: 'User with the given id  Not Found' })
         }
 
         videoCount.commentCount++;
@@ -72,8 +69,7 @@ exports.comment = async(req, res) => {
         await UserVideo.findOneAndUpdate({ _id: ObjectId(video_id)}, 
         {$set: {"videodetail": videoCount._id}} , {new : true});
 
-        res.status(201).send({ status: "success" })
-
+        res.status(201).send({ success: true })
     } 
     catch (error) {
         console.log(error);
